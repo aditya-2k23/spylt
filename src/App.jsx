c5781a8df08d34a94f5180ff0550a92b7c868c8c
aditya-2k23 <holaaditya123@gmail.com>
2025-07-08 14:49:38 +0530

    Add Hero component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 };
