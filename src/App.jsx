32725acc1bc495bbd61b0012143bb7ab186a44dc
aditya-2k23 <holaaditya123@gmail.com>
2025-07-08 18:15:32 +0530

    Enhance Hero component with GSAP animations and scroll effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <div className="h-dvh border border-red-500"></div>
     </main>
   );
 };
